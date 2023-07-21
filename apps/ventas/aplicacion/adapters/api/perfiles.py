b01846d8954c0320b1e74f06ef897e039301b4cd
Jesus Antonio Castañeda Valencia <jesus.a.castaneda.v@gmail.com>
2023-07-21 00:18:36 -0500

    Tipado del lenguaje

--- apps/ventas/aplicacion/adapters/api/perfiles.py
+++ apps/ventas/aplicacion/adapters/api/perfiles.py
@@ -1,9 +1,10 @@
 from django.db import transaction
-from typing import Any, List
+from typing import List
 
 from rest_framework import status
 from rest_framework.permissions import BasePermission, IsAuthenticated
 from rest_framework.views import APIView
+from rest_framework.request import Request
 from rest_framework.response import Response
 
 from apps.ventas.aplicacion.adapters.api.serializers.perfiles import PerfilSerializer
@@ -16,12 +17,12 @@ class PerfilAdapter(APIView):
     permission_classes: List[BasePermission] = [IsAuthenticated]
     perfil_use_case: PerfilUseCase = None
 
-    def get(self, request):
+    def get(self, request: Request) -> Response:
         perfiles = self.perfil_use_case.list_perfiles()
         perfiles_serializer = PerfilSerializer(perfiles, many=True)
         return Response(perfiles_serializer.data)
 
-    def post(self, request):
+    def post(self, request: Request) -> Response:
         with transaction.atomic():
             try:
                 usuario_serializer = UsuarioSerializer(data=request.data)
@@ -39,14 +40,14 @@ class PerfilAdapter(APIView):
                 raise e
         return Response(data, status=status.HTTP_201_CREATED)
 
-    def patch(self, request, perfil_id):
+    def patch(self, request: Request, perfil_id: int) -> Response:
         perfil = self.perfil_use_case.get_perfil(perfil_id)
         perfil_serializer = PerfilSerializer(instance=perfil, data=request.data, partial=True)
         perfil_serializer.is_valid(raise_exception=True)
         perfil_serializer.save()
         return Response(perfil_serializer.data, status=status.HTTP_200_OK)
 
-    def delete(self, request, perfil_id):
+    def delete(self, request: Request, perfil_id: int) -> Response:
         usuario_id = self.perfil_use_case.delete_perfil(perfil_id)
         usuario_use_case = UsuarioUseCase()
         usuario_use_case.delete_usuario(usuario_id)
